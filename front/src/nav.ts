93eb9fdced068cb14f9dfe2724b433c823a56251
whguswo <youjhj88@gmail.com>
2021-06-26 06:16:33 +0100

    디자인중

--- front/src/nav.ts
+++ front/src/nav.ts
@@ -1,5 +1,6 @@
 export const init = () => {
     const nav = document.querySelector('.navigation')
+    const main = document.querySelector('#main')
     nav.addEventListener('click', (e) => {
         nav.classList.toggle('active')
     })
