9f063ab78d48fd966e485659f2c766ad81c616b2
whguswo <youjhj88@gmail.com>
2021-08-15 07:44:48 +0100

    CSS media query 추가

--- front/src/code.ts
+++ front/src/code.ts
@@ -9,6 +9,18 @@ const phoneNum = document.querySelector<HTMLInputElement>('#phone')
 const button = document.querySelector<HTMLButtonElement>('#makeQR')
 const code = document.querySelector<HTMLCanvasElement>('#code')
 
+const ctx = code.getContext('2d');
+let img = new Image ();
+img.src = "../src/qrcode.png" ;
+code.width = 164;
+code.height = 164;
+code.style.width = '200px';
+code.style.height = '200px';
+img.onload = () => {
+    ctx.globalAlpha = 0.2;
+    ctx.drawImage(img, 0, 0, 164, 164);
+};
+
 button.addEventListener('click', async(e) => {
     if(qrName.value != '' && phoneNum.value != '') {
         const result = await fetch('/qrHash', {
